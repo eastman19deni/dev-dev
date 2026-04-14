f435b678bd0b20382c26d3584f11f24026bcdc0a
Deni <deni12349075@gmail.com>
2026-04-14 23:27:45 +0300

    deleted rollup

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig, loadEnv } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rollup/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
@@ -12,7 +11,6 @@ export default defineConfig(({ mode }) => {
     plugins: [
       tailwindcss(),
       react(),
-      babel({ presets: [reactCompilerPreset()] }),
     ],
     server: {
       proxy: {
